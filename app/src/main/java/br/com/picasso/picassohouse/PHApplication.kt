f521cc3f3c2a359140d3e5268508c8ee49a001f3
Antony Alkmim <antony-alkmim@hotmail.com>
2017-03-27 09:26:23 -0300

    setup leakcanary

--- app/src/main/java/br/com/picasso/picassohouse/PHApplication.kt
+++ app/src/main/java/br/com/picasso/picassohouse/PHApplication.kt
@@ -1,12 +1,18 @@
 package br.com.picasso.picassohouse
 
 import android.app.Application
+import com.squareup.leakcanary.LeakCanary
 
 
 class PHApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            return
+        }
+        LeakCanary.install(this)
     }
 
 }
