98072799bcce8d21518d81956ac052e40ae00d84
Antony Alkmim <antony-alkmim@hotmail.com>
2017-04-13 11:30:50 -0300

    add room icons

--- app/src/main/java/br/com/picasso/picassohouse/models/RoomType.kt
+++ app/src/main/java/br/com/picasso/picassohouse/models/RoomType.kt
@@ -3,15 +3,16 @@ package br.com.picasso.picassohouse.models
 import br.com.picasso.picassohouse.R
 
 enum class RoomType {
-    bedRoom, livingRoom, kitchen, garage;
+    bedRoom, livingRoom, kitchen, garage, bath;
 
     fun iconRes() : Int {
         //TODO: change icons
         when (this) {
-            bedRoom -> return R.drawable.ic_garage
-            kitchen -> return R.drawable.ic_garage
-            livingRoom -> return R.drawable.ic_garage
+            bedRoom -> return R.drawable.ic_bed
+            kitchen -> return R.drawable.ic_kitchen
+            livingRoom -> return R.drawable.ic_living_room
             garage -> return R.drawable.ic_garage
+            bath -> return R.drawable.ic_bath
         }
     }
 }
